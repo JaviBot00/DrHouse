29570126cbf5874f02b27f3370223481649be395
HotGuy <javibot00@gmail.com>
2026-06-15 20:52:42 +0200

    [master] feat: Initialize DrHouse-ImportDataClient project with Gradle setup and core functionality - Added build.gradle file for project configuration and dependencies. - Included Gradle wrapper files for consistent builds. - Created main application structure with Main class and entry point. - Implemented CSVParser for parsing patient, treatment, and medication data. - Developed ImportController to handle data import and HTTP requests. - Added model classes for Medicamento, Paciente, Tratamiento, and MedicamentoTratamiento. - Created MainView for user interaction and data import initiation. - Removed old project files and configurations from previous structure.

--- importDataClient/importDataClient/src/main/java/com/politecnicomalaga/importcsv/Main.java
+++
@@ -1,9 +0,0 @@
-package com.politecnicomalaga.importcsv;
-
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.printf("Crea el código necesario para abrir el fichero CSV y mandar los datos al server!");
-
-    }
-}
